0c10e6b59aaba8d69cf4844e7995d18a775fa39f
Kseniya Tulikova <ksenia77383@gmail.com>
2018-11-15 23:19:57 +0200

    LuxuryRestaraunt Site
    Added my website

---
+++ script/script.js
@@ -0,0 +1,61 @@
+$(function() {
+    // $(".handler span").on("click", function() {
+    //     $(this).addClass("choosen").siblings().removeClass("choosen");
+    //     $(".slide")
+    //         .eq($(this).index())
+    //         .addClass("show")
+    //         .siblings()
+    //         .removeClass("show");
+    // });
+
+    $(".slide").on("click", function() {
+        var images = $(".slide");
+        images.siblings().removeClass("show");
+        var newIndex = images.index(this) < images.length-1 ? images.index(this)+1 : 0;
+        images.eq(newIndex).addClass("show");
+    });
+    
+    $(document).keydown(function(e) {
+        var code = e.keyCode || e.which;
+        if (code === 40 || code === 39) {
+            if ($(".handler span:last-child").hasClass("choosen")) {
+                $(".handler span")
+                    .eq(0)
+                    .addClass("choosen")
+                    .siblings()
+                    .removeClass("choosen");
+            } else {
+                $(".choosen").removeClass("choosen").next().addClass("choosen");
+            }
+            $(".slide")
+                .eq($(".choosen").index())
+                .addClass("show")
+                .siblings()
+                .removeClass("show");
+        } else {
+            if (code === 38 || code === 37) {
+                if ($(".handler span:first-child").hasClass("choosen")) {
+                    $(".handler span:last-child")
+                        .addClass("choosen")
+                        .siblings()
+                        .removeClass("choosen");
+                } else {
+                    $(".choosen").removeClass("choosen").prev().addClass("choosen");
+                }
+                $(".slide")
+                    .eq($(".choosen").index())
+                    .addClass("show")
+                    .siblings()
+                    .removeClass("show");
+            }
+        }
+    });
+    
+});
+
+$(function(){
+      $('.icon').click(function(){
+        event.preventDefault(),
+        $('.topnav').slideToggle(1000);
+      });
+    });
